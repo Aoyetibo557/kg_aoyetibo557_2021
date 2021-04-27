a3a42d3f3294c82b4a7724e2208d814455f0f2f3
aoyetibo557 <aoyetibo@gmail.com>
2021-04-27 04:49:18 -0400

    Added two functions, toWords - returns the string equivalnat of the input number and splitToDigit- splits a given number into an array

--- main.js
+++ main.js
@@ -2,4 +2,46 @@
  * Anuoluwapo Oyetibo
  * 04/27/21
  */
-console.log("First commit into github repo")
+
+/**
+ * This function takes in a value and uses a switch statement to determine the 
+ * appropriate return value
+ * @param {*} val - input value  
+ * @returns the string equivalant of the input number
+ */
+function toWords(val){
+    switch (val) {
+        case 0:
+            return "Zero"
+        case 1:
+            return "One";
+        case 2:
+            return "Two";
+        case 3: 
+            return "Three" 
+        case 4:
+            return "Four";
+        case 5:
+            return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+            
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+        default:
+            break;
+    }
+}
+
+/**
+ * SplitToDigit Function takes a number and seperates them into a new array
+ * @param {input number} n 
+ * @returns seperated number
+ */
+function splitToDigit(n){
+    return [...n + ''].map(Number);
+}
